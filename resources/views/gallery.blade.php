0a45627bff2213466ca778ddba6c35519beec36c
ConradoSM <conradomaranguello@gmail.com>
2018-12-11 02:06:47 -0300

    V2.0 BETA Primer Commit

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -4,12 +4,12 @@
 @forelse ($gallery as $pub)
 <div class="pubContainer">
 <article class="pub_galeria">
+    <p class="info"><img class="svg" src="{{ asset('svg/calendar.svg') }}" /> {{ $pub -> fecha }}</p>
     <h2>{{ $pub -> titulo }}</h2>
     <p class="copete">{{ $pub -> copete }}</p>
     <p class="info">
     <img class="svg" src="{{ asset('svg/user_black.svg') }}" /> {{ $pub -> autor }}
     <img class="svg" src="{{ asset('svg/eye.svg') }}" /> {{ $pub -> visitas }} lecturas
-    <img class="svg" src="{{ asset('svg/clock-circular-outline.svg') }}" /> {{ $pub -> fecha }}
     </p>
     <hr />
 </article>
@@ -22,7 +22,6 @@
     <img src="{{ asset('img/articles/'.$foto -> foto) }}" />
     <p>{{ $foto -> descripcion_foto }}</p>
 </article>
-<hr />
 @empty
     <h1>No hay artículos para mostrar</h1>
 @endforelse
