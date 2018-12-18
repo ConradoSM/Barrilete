7974d6ad1ec0e7bd9bea592b526a3360380baf68
ConradoSM <conradomaranguello@gmail.com>
2018-12-17 22:09:42 -0300

    V2.0 BETA - normalización de las tablas para las migraciones

--- database/migrations/2014_10_12_600000_create_gallery_table.php
+++ database/migrations/2014_10_12_600000_create_gallery_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateGalleryTable extends Migration
 {
     /**
      * Run the migrations.
@@ -23,8 +23,8 @@ class CreateUsersTable extends Migration
             $table->string('seccion');
             $table->integer('author');
             $table->string('article_desc');
-            $table->boolean('publish')->default(0);
-            $table->integer('views')->default(0);
+            $table->boolean('publish');
+            $table->integer('views');
             $table->rememberToken();
             $table->timestamps();
         });
