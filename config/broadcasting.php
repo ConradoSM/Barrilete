d2cbfc1bc4abb234d5f1ab56af0c128f75ac0f29
Conrado <cmaranguello@summasolutions.net>
2020-03-01 18:22:04 -0300

    add config websockets to dev server

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,14 +37,10 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => true,
+                'encrypted' => false,
                 'host' => env('LARAVEL_WEBSOCKETS_HOST'),
                 'port' => env('LARAVEL_WEBSOCKETS_PORT'),
-                'scheme' => env('LARAVEL_WEBSOCKETS_SCHEME'),
-                'curl_options' => [
-                    CURLOPT_SSL_VERIFYHOST => 0,
-                    CURLOPT_SSL_VERIFYPEER => 0,
-                ]
+                'scheme' => env('LARAVEL_WEBSOCKETS_SCHEME')
             ],
         ],
 
